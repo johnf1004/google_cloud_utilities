3ab6f634259c2fad0ad419c2b6f8b930bb7ce642
John Foley <johnf1004@yahoo.co.uk>
2026-01-04 18:04:38 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ URL = 'https://github.com/johnf1004/google_cloud_utilities'
 EMAIL = 'johnf1004@yahoo.co.uk'
 AUTHOR = 'John Foley'
 REQUIRES_PYTHON = '>=3.9.7'
-VERSION = '0.8.8'
+VERSION = '0.8.9'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
